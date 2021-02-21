9c4bb581249d865b0e55fc0dd6efbc0fb5d36cd4
MaPePeR <MaPePeR@users.noreply.github.com>
2021-02-21 20:30:04 +0100

    Form Fields for Title, Description and Tags for new Project Modal

--- src/index.js
+++ src/index.js
@@ -35,6 +35,26 @@ function initializeBibtab() {
     }
 }
 document.getElementById('bib-tab').addEventListener('show.bs.tab', initializeBibtab);
+
+
+function initializeNewProjectModal() {
+    let tagDiv = document.getElementById('newProjectModalTagsDiv');
+    let template = document.getElementById('newProjectModalTagElementTemplate');
+    let label = template.content.querySelector('label');
+    let input = template.content.querySelector('input');
+    let fragment = document.createDocumentFragment();
+    for (let tag of config.projecttags) {
+        label.innerText = tag;
+        label.for = 'newProjectModalTag' + tag;
+        input.id = label.for;
+        input.value = tag;
+        fragment.appendChild(document.importNode(template.content, true));
+    }
+    tagDiv.replaceChildren(fragment);
+}
+initializeNewProjectModal();
+
+
 function switchTab(tab) {
     if (tab == '#intro') {
         bootstrap.Tab.getInstance(document.querySelector('#tabs a[data-bs-target="#intro-tab-pane"]')).show();
